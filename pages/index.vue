0573849b3ebc2fa76d4d2d6fd0342183164cd2bc
Dennis Nichiporenko <d.nichiporenko@itspecial.net>
2020-08-21 09:41:32 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+ <section class="home-page">
+   <section class="intro">
+     <h1>Get the tech news</h1>   
+   </section>
+   
+   <section class="featured-posts ">
+    <PostPreview 
+      id="1"
+      title="Name 1"
+      previewText="previewText previewText"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+    />
+   
+     <PostPreview 
+      id="2"
+      title="Name 2"
+      previewText="previewText previewText"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+    />
+
+     <PostPreview 
+      id="2"
+      title="Name 2"
+      previewText="previewText previewText"
+      thumbnail="https://images.pexels.com/photos/270348/pexels-photo-270348.jpeg"
+    />
+
+   </section>
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  }
+}
+</script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+  background-image: url('~assets/images/main-page-background.jpg');
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <section class="post">
+        <h1 class="post-title">Title of the post</h1>
+        <div class="post-detail">
+            <div> Last update</div>
+            <div>Written by NAME</div>
+        </div>
+        <p>Post content</p>
+
+    </section>
+</template>
+
+<style  scoped>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
+</style>
